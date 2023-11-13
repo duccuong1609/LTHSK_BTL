157dc9029e8d86dd844dc59105b43701c29298d2
Nguyen Duc Cuong <duccuong1609@gmail.com>
2023-11-13 18:47:49 +0700

    fix regex

--- Project_Eclipse/BTL_HSK/src/GUI/UI_TC_NV.java
+++ Project_Eclipse/BTL_HSK/src/GUI/UI_TC_NV.java
@@ -45,7 +45,7 @@ public class UI_TC_NV implements MouseListener,ActionListener{
 	private JTextField NV_txt_Email;
 	
 //	private DanhSachPhieuDat phieuDat;
-//	private Pattern name = Pattern.compile("([A-Z][a-z]+ )+([A-Z][a-z]+)");
+	private Pattern name = Pattern.compile("([^\\.~\\*!`@#$%^&*()_+={}\\[\\]/><,;:'\"|\\-?/\\d]+)");
 	private Pattern email = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
 	private Pattern phone = Pattern.compile("(84|0[3|5|7|8|9])+([0-9]{8})");
 	
@@ -360,12 +360,12 @@ public class UI_TC_NV implements MouseListener,ActionListener{
 			NV_txt_Email.requestFocus();
 			return false;
 		}
-//		if(!name.matcher(NV_txt_TenNV.getText()).matches()) {
-//			JOptionPane.showMessageDialog(display_NV, "Họ Tên Gồm Từ 2 Từ Trở Lên. Mỗi Từ Có Chữ Cái Đầu Viết Hoa !");
-//			NV_txt_TenNV.setText("");
-//			NV_txt_TenNV.requestFocus();
-//			return false;
-//		}
+		if(!name.matcher(NV_txt_TenNV.getText()).matches()) {
+			JOptionPane.showMessageDialog(display_NV, "Họ Tên Gồm Từ 2 Từ Trở Lên. Mỗi Từ Có Chữ Cái Đầu Viết Hoa !");
+			NV_txt_TenNV.setText("");
+			NV_txt_TenNV.requestFocus();
+			return false;
+		}
 		if(!email.matcher(NV_txt_Email.getText()).matches()) {
 			JOptionPane.showMessageDialog(display_NV, "Sai Định Dạng Email !");
 			NV_txt_Email.setText("");;
